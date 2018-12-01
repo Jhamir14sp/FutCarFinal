f3f723dbc066e4bf80ac6d505ddfc420b01e07f2
Jhamir Saenz <jhamirsaenz@gmail.com>
2018-12-01 17:08:31 -0500

    ok

--- app/src/main/java/com/example/invitado2/futurecar/http.java
+++ app/src/main/java/com/example/invitado2/futurecar/http.java
@@ -15,7 +15,7 @@ import java.net.URL;
 
 public class http {
 
-    private String ip = "192.168.43.243.:10";
+    private String ip = "192.168.43.243:10";
 
     public String downloadUrl(String myurl) throws IOException{
         Log.i("URL",""+myurl);
